92d4e54154338aaade362708783dfd47918bbb59
Bryan Phelps <bryphe@outlook.com>
2018-10-02 18:11:20 -0700

    Minimal repro

--- lib/Util.ml
+++ lib/Util.ml
@@ -1,5 +1,12 @@
 open LTerm_style
 open LTerm_text
 
+
+let%expect_test "addition" =
+    print_endline "4";
+    [%expect {|
+    a
+|}]
+
 let hello () =
   LTerm.printls (eval [B_fg(red); S"Hello,"; E_fg; S" "; B_fg(green); S"World!"; E_fg])
