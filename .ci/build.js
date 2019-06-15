fd4e8c0eff3407888741c987b42e939778a61e6a
Andrey Popp <8mayday@gmail.com>
2019-06-15 12:44:23 +0300

    up

---
+++ .ci/build.js
@@ -0,0 +1,63 @@
+let child_process = require('child_process');
+let process = require('process');
+
+class RunCommandError extends Error {
+  constructor(code) {
+    super(`Command failed with exit code: ${code}`);
+  }
+}
+
+function waitForChildProcess(child) {
+  return new Promise((resolve, reject) => {
+    child.addListener("error", reject);
+    child.addListener("exit", (code) => {
+      if (code !== 0) {
+        reject(new RunCommandError('ccmd', code));
+      } else {
+        resolve();
+      }
+    });
+  });
+}
+
+function run(...line) {
+  let [cmd, ...args] = line;
+  let cp = child_process.spawn(cmd, args, {stdio: 'inherit'});
+  return waitForChildProcess(cp)
+}
+
+ESY_VERSION = 'latest'
+
+let commands = {
+  npmInstallEsy: async () => {
+    let esy = `esy@${ESY_VERSION}`;
+    await run('npm', 'install', '--global', esy);
+  },
+  esyInstall: async () => {
+    await run('esy', 'install');
+  },
+  esyBuild: async () => {
+    await run('esy', 'build');
+  },
+}
+
+function error(msg) {
+  console.error(msg);
+  process.exit(1);
+}
+
+async function main() {
+  let [commandName] = process.argv.slice(2);
+  if (commandName == null || !commands.hasOwnProperty(commandName)) {
+    error(`No command provided, available: ${Object.keys(commands).join(', ')}`);
+  }
+  let command = commands[commandName];
+  await command();
+}
+
+main().catch((err) => {
+  if (!(err instanceof RunCommandError)) {
+    console.error(err);
+  }
+  process.exit(1);
+});
